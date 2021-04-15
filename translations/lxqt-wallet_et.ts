3e99460748ccccf235024298ce9bac2e58dbb4f5
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-04-15 07:53:45 +0200

    Translations update from Weblate (#34)
    * Weblate commits
    
    Co-authored-by: Heimen Stoffels <vistausss@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/nl/
    Translation: LXQt Desktop/LXQt Wallet
    
    * Weblate commits
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/et/
    Translation: LXQt Desktop/LXQt Wallet
    
    Co-authored-by: Heimen Stoffels <vistausss@outlook.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>

--- translations/lxqt-wallet_et.ts
+++ translations/lxqt-wallet_et.ts
@@ -1,38 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl">
+<TS version="2.1" language="et">
 <context>
     <name>LXQt::Wallet::changePassWordDialog</name>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="108"/>
         <source>Create a new wallet</source>
-        <translation>Nieuwe sleutelbos aanmaken</translation>
+        <translation>Loo uus turvalaegas</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="110"/>
         <source>Create</source>
-        <translation>Aanmaken</translation>
+        <translation>Loo</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="138"/>
         <location filename="../frontend/changepassworddialog.cpp" line="280"/>
         <source>Passwords do not match</source>
-        <translation>De wachtwoorden komen niet overeen</translation>
+        <translation>Salasõnad ei klapi</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="221"/>
         <source>Wallet password could not be changed</source>
-        <translation>Het sleutelboswachtwoord kan niet worden gewijzigd</translation>
+        <translation>Turvalaeka salasõna ei õnnestunud muuta</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="232"/>
         <source>Wallet could not be opened with the presented key</source>
-        <translation>De sleutelbos kan niet ontgrendeld worden met de opgegeven sleutel</translation>
+        <translation>Sisestatud võtmega ei õnnestunud turvalaegast avada</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="244"/>
         <source>New passwords do not match</source>
-        <translation>De nieuwe wachtwoorden komen niet overeen</translation>
+        <translation>Uued salasõnad ei klapi omavahel</translation>
     </message>
 </context>
 <context>
@@ -40,7 +40,7 @@
     <message>
         <location filename="../frontend/password_dialog.cpp" line="72"/>
         <source>Wallet could not be opened with the presented key</source>
-        <translation>De sleutelbos kan niet ontgrendeld worden met de opgegeven sleutel</translation>
+        <translation>Sisestatud võtmega ei õnnestunud turvalaegast avada</translation>
     </message>
 </context>
 <context>
@@ -48,37 +48,37 @@
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="14"/>
         <source>Change Wallet&apos;s Password</source>
-        <translation>Sleutelboswachtwoord wijzigen</translation>
+        <translation>Muuda turvalaeka salasõna</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="42"/>
         <source>C&amp;hange</source>
-        <translation>&amp;Wijzigen</translation>
+        <translation>&amp;Muuda</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="55"/>
         <source>&amp;Ok</source>
-        <translation>&amp;Oké</translation>
+        <translation>&amp;Sobib</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="126"/>
         <source>Enter Current Password Below.</source>
-        <translation>Voer het huidige wachtwoord hieronder in.</translation>
+        <translation>Alljärgnevalt sisesta kehtiv salasõna.</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="145"/>
         <source>Enter New Password Below.</source>
-        <translation>Voer het nieuwe wachtwoord hieronder in.</translation>
+        <translation>Alljärgnevalt sisesta uus salasõna.</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="164"/>
         <source>Re Enter New Password Below.</source>
-        <translation>Voer het nieuwe wachtwoord hieronder nogmaals in.</translation>
+        <translation>Alljärgnevalt sisesta uus salasõna uuesti.</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="183"/>
         <source>&amp;Cancel</source>
-        <translation>&amp;Annuleren</translation>
+        <translation>&amp;Katkesta</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="196"/>
@@ -92,7 +92,7 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Een programma &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; heeft een verzoek ingediend om een wachtwoord in de sleutelbos &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; te wijzigen&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;„&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;“ rakendus soovib muuta „&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;“ turvalaekasse salvestatud salasõna&lt;/p&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
@@ -106,7 +106,7 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Voer hieronder een wachtwoord in om een sleutelbos aan te maken &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; voor het programma &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Uue „&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;“ loomiseks rakendusele „&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;“ sisesta salasõna alljärgnevalt&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -114,17 +114,17 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../frontend/password_dialog.ui" line="14"/>
         <source>lxqt wallet service</source>
-        <translation>lxqt-sleutelbosdienst</translation>
+        <translation>lxqt turvalaeka teenus</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="42"/>
         <source>The application &apos;%1&apos; has requested to open the wallet &apos;%2&apos;.Enter the password below for this wallet to unlock it</source>
-        <translation>Het programma &apos;%1&apos; heeft een verzoek ingediend om de sleutelbos &apos;%2&apos; te ontgrendelen. Voer het wachtwoord hiervan in om deze te ontgrendelen.</translation>
+        <translation>„%1“ rakendus soovib avada turvalaegast „%2“. Selleks palun sisesta turvalaeka salsaõna</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="74"/>
         <source>Wallet &apos;%1&apos; does not exist,do you want to create it?</source>
-        <translation>De sleutelbos &apos;%1&apos; bestaat niet. Wilt u een nieuwe aanmaken?</translation>
+        <translation>„%1“ turvalaegast pole olemas. Kas sa soovid seda luua?</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="90"/>
@@ -137,7 +137,7 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;De sleutelbos &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; bestaat niet. Wilt u een nieuwe aanmaken?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; turvalaegast pole olemas. Kas sa soovid seda luua?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="107"/>
@@ -150,27 +150,27 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Een programma &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; heeft een verzoek ingediend om een sleutelbos te ontgrendelen&lt;span style=&quot; font-weight:600;&quot;&gt; &apos;%2&apos;&lt;/span&gt;.Voer het wachtwoord hiervan in om deze te ontgrendelen&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;„%1“&lt;/span&gt; rakendus soovib avada turvalaegast &lt;span style=&quot; font-weight:600;&quot;&gt; „%2“&lt;/span&gt;. Selleks palun sisesta antud turvalaeka salsaõna.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="124"/>
         <source>&amp;Unlock</source>
-        <translation>&amp;Ontgrendelen</translation>
+        <translation>Võta lukust &amp;lahti</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="141"/>
         <source>&amp;Cancel</source>
-        <translation>&amp;Annuleren</translation>
+        <translation>&amp;Katkesta</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="167"/>
         <source>&amp;Ok</source>
-        <translation>&amp;Oké</translation>
+        <translation>&amp;Sobib</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="154"/>
         <source>ok</source>
-        <translation>Oké</translation>
+        <translation>sobib</translation>
     </message>
 </context>
 </TS>
