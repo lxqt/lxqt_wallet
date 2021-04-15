045c3edb948cb9a6339790e761700b17db635d70
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-15 07:51:26 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/et/
    Translation: LXQt Desktop/LXQt Wallet

---
+++ translations/lxqt-wallet_et.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LXQt::Wallet::changePassWordDialog</name>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="108"/>
+        <source>Create a new wallet</source>
+        <translation>Loo uus turvalaegas</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="110"/>
+        <source>Create</source>
+        <translation>Loo</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="138"/>
+        <location filename="../frontend/changepassworddialog.cpp" line="280"/>
+        <source>Passwords do not match</source>
+        <translation>Salasõnad ei klapi</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="221"/>
+        <source>Wallet password could not be changed</source>
+        <translation>Turvalaeka salasõna ei õnnestunud muuta</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="232"/>
+        <source>Wallet could not be opened with the presented key</source>
+        <translation>Sisestatud võtmega ei õnnestunud turvalaegast avada</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="244"/>
+        <source>New passwords do not match</source>
+        <translation>Uued salasõnad ei klapi omavahel</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::Wallet::password_dialog</name>
+    <message>
+        <location filename="../frontend/password_dialog.cpp" line="72"/>
+        <source>Wallet could not be opened with the presented key</source>
+        <translation>Sisestatud võtmega ei õnnestunud turvalaegast avada</translation>
+    </message>
+</context>
+<context>
+    <name>changePassWordDialog</name>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="14"/>
+        <source>Change Wallet&apos;s Password</source>
+        <translation>Muuda turvalaeka salasõna</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="42"/>
+        <source>C&amp;hange</source>
+        <translation>&amp;Muuda</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="55"/>
+        <source>&amp;Ok</source>
+        <translation>&amp;Sobib</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="126"/>
+        <source>Enter Current Password Below.</source>
+        <translation>Alljärgnevalt sisesta kehtiv salasõna.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="145"/>
+        <source>Enter New Password Below.</source>
+        <translation>Alljärgnevalt sisesta uus salasõna.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="164"/>
+        <source>Re Enter New Password Below.</source>
+        <translation>Alljärgnevalt sisesta uus salasõna uuesti.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="183"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Katkesta</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="196"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;An application &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; has made a request for a password of its wallet &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; to be changed&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;„&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;“ rakendus soovib muuta „&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;“ turvalaekasse salvestatud salasõna&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="214"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Enter password information below to create a new wallet &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; for application &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Uue „&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;“ loomiseks rakendusele „&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;“ sisesta salasõna alljärgnevalt&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>password_dialog</name>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="14"/>
+        <source>lxqt wallet service</source>
+        <translation>lxqt turvalaeka teenus</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="42"/>
+        <source>The application &apos;%1&apos; has requested to open the wallet &apos;%2&apos;.Enter the password below for this wallet to unlock it</source>
+        <translation>„%1“ rakendus soovib avada turvalaegast „%2“. Selleks palun sisesta turvalaeka salsaõna</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="74"/>
+        <source>Wallet &apos;%1&apos; does not exist,do you want to create it?</source>
+        <translation>„%1“ turvalaegast pole olemas. Kas sa soovid seda luua?</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="90"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Wallet &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; does not exist, do you want to create it?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; turvalaegast pole olemas. Kas sa soovid seda luua?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="107"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;An application &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; has requested to open a wallet&lt;span style=&quot; font-weight:600;&quot;&gt; &apos;%2&apos;&lt;/span&gt;. Enter the password below for this wallet to unlock it.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;„%1“&lt;/span&gt; rakendus soovib avada turvalaegast &lt;span style=&quot; font-weight:600;&quot;&gt; „%2“&lt;/span&gt;. Selleks palun sisesta antud turvalaeka salsaõna.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="124"/>
+        <source>&amp;Unlock</source>
+        <translation>Võta lukust &amp;lahti</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="141"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Katkesta</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="167"/>
+        <source>&amp;Ok</source>
+        <translation>&amp;Sobib</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="154"/>
+        <source>ok</source>
+        <translation>sobib</translation>
+    </message>
+</context>
+</TS>
