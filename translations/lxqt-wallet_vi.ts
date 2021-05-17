2da24fa826e7c93cdaa4278a74cb74300ca86474
dimuaconheo <dimuaconheo@gmail.com>
2021-05-17 07:07:22 +0200

    Weblate commits
    Co-authored-by: dimuaconheo <dimuaconheo@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/vi/
    Translation: LXQt Desktop/LXQt Wallet

---
+++ translations/lxqt-wallet_vi.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi">
+<context>
+    <name>LXQt::Wallet::changePassWordDialog</name>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="108"/>
+        <source>Create a new wallet</source>
+        <translation>Tạo một ví mới</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="110"/>
+        <source>Create</source>
+        <translation>Tạo</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="138"/>
+        <location filename="../frontend/changepassworddialog.cpp" line="280"/>
+        <source>Passwords do not match</source>
+        <translation>Mật khẩu không trùng nhau</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="221"/>
+        <source>Wallet password could not be changed</source>
+        <translation>Mật khẩu ví không thể thay đổi được</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="232"/>
+        <source>Wallet could not be opened with the presented key</source>
+        <translation>Ví không thể mở được với key đã cho</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="244"/>
+        <source>New passwords do not match</source>
+        <translation>Mật khẩu mới không trùng nhau</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::Wallet::password_dialog</name>
+    <message>
+        <location filename="../frontend/password_dialog.cpp" line="72"/>
+        <source>Wallet could not be opened with the presented key</source>
+        <translation>Ví không thể mở được với key đã cho</translation>
+    </message>
+</context>
+<context>
+    <name>changePassWordDialog</name>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="14"/>
+        <source>Change Wallet&apos;s Password</source>
+        <translation>Thay đổi Mật khẩu Ví</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="42"/>
+        <source>C&amp;hange</source>
+        <translation>T&amp;hay đổi</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="55"/>
+        <source>&amp;Ok</source>
+        <translation>&amp;Ok</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="126"/>
+        <source>Enter Current Password Below.</source>
+        <translation>Nhập Mật khẩu Hiện tại Ở dưới.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="145"/>
+        <source>Enter New Password Below.</source>
+        <translation>Nhập Mật khẩu Mới Ở dưới.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="164"/>
+        <source>Re Enter New Password Below.</source>
+        <translation>Nhập Lại Mật khẩu Mới Ở dưới.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="183"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Hủy</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="196"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;An application &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; has made a request for a password of its wallet &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; to be changed&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Một ứng dụng &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; đã gửi một yêu cầu cho một mật khẩu của ví của nó &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; được thay đổi&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="214"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Enter password information below to create a new wallet &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; for application &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Nhập thông tin mật khẩu ở dưới để tạo một ví mới &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; cho ứng dụng &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>password_dialog</name>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="14"/>
+        <source>lxqt wallet service</source>
+        <translation>dịch vụ ví lxqt</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="42"/>
+        <source>The application &apos;%1&apos; has requested to open the wallet &apos;%2&apos;.Enter the password below for this wallet to unlock it</source>
+        <translation>Ứng dụng &apos;%1&apos; yêu cầu mở ví &apos;%2&apos;.Nhập mật khẩu ở dưới cho ví này để mở khóa nó</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="74"/>
+        <source>Wallet &apos;%1&apos; does not exist,do you want to create it?</source>
+        <translation>Ví &apos;%1&apos; không tồn tại,bạn có muốn tạo nó?</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="90"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Wallet &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; does not exist, do you want to create it?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Ví &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; không tồn tại, bạn có muốn tạo nó?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="107"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;An application &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; has requested to open a wallet&lt;span style=&quot; font-weight:600;&quot;&gt; &apos;%2&apos;&lt;/span&gt;. Enter the password below for this wallet to unlock it.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Một ứng dụng &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; yêu cầu mở một ví&lt;span style=&quot; font-weight:600;&quot;&gt; &apos;%2&apos;&lt;/span&gt;. Nhập mật khẩu ở dưới để mở khóa nó.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="124"/>
+        <source>&amp;Unlock</source>
+        <translation>&amp;Mở khóa</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="141"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Hủy</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="167"/>
+        <source>&amp;Ok</source>
+        <translation>&amp;Ok</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="154"/>
+        <source>ok</source>
+        <translation>ok</translation>
+    </message>
+</context>
+</TS>
