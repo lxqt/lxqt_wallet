c6fe3ead00a8084bf88b30aaa9839e919dd97990
Weblate <noreply@weblate.org>
2022-10-24 14:10:31 +0200

    Weblate commits
    Co-authored-by: Ali Alhaidary <ali.alhaidary@the5stars.org>
    Co-authored-by: Bashishtha Narayan Singh <bashishtha.singh@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: علي الحيدري <ali.alhaidary@the5stars.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/hi/
    Translation: LXQt Desktop/LXQt Wallet

---
+++ translations/lxqt-wallet_ar.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>LXQt::Wallet::changePassWordDialog</name>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="108"/>
+        <source>Create a new wallet</source>
+        <translation>أنشئ محفظة جديدة</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="110"/>
+        <source>Create</source>
+        <translation>أنشئ</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="138"/>
+        <location filename="../frontend/changepassworddialog.cpp" line="280"/>
+        <source>Passwords do not match</source>
+        <translation>كلمات المرور غير مطابقة</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="221"/>
+        <source>Wallet password could not be changed</source>
+        <translation>لا يمكن تغيير كلمة مرور المحفظة</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="232"/>
+        <source>Wallet could not be opened with the presented key</source>
+        <translation>تعذر فتح المحفظة بالمفتاح المقدم</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.cpp" line="244"/>
+        <source>New passwords do not match</source>
+        <translation>كلمات المرور الجديدة لا تتطابق</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::Wallet::password_dialog</name>
+    <message>
+        <location filename="../frontend/password_dialog.cpp" line="72"/>
+        <source>Wallet could not be opened with the presented key</source>
+        <translation>تعذر فتح المحفظة بالمفتاح المقدم</translation>
+    </message>
+</context>
+<context>
+    <name>changePassWordDialog</name>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="14"/>
+        <source>Change Wallet&apos;s Password</source>
+        <translation>تغيير كلمة مرور المحفظة</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="42"/>
+        <source>C&amp;hange</source>
+        <translation>&amp;غيّر</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="55"/>
+        <source>&amp;Ok</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="126"/>
+        <source>Enter Current Password Below.</source>
+        <translation>أدخل كلمة المرور الحالية أدناه.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="145"/>
+        <source>Enter New Password Below.</source>
+        <translation>أدخل كلمة المرور الجديدة أدناه.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="164"/>
+        <source>Re Enter New Password Below.</source>
+        <translation>أعد إدخال كلمة المرور الجديدة أدناه.</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="183"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;إلغِ</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="196"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;An application &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; has made a request for a password of its wallet &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; to be changed&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;طلب أحد التطبيقات &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos;بتقديم طلب للحصول على كلمة مرور لمحفظته &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;لتتغير&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/changepassworddialog.ui" line="214"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Enter password information below to create a new wallet &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; for application &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;أدخل معلومات كلمة المرور أدناه لإنشاء محفظة جديدة &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; للتطبيق &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>password_dialog</name>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="14"/>
+        <source>lxqt wallet service</source>
+        <translation>خدمة المحفظة lxqt</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="42"/>
+        <source>The application &apos;%1&apos; has requested to open the wallet &apos;%2&apos;.Enter the password below for this wallet to unlock it</source>
+        <translation>طلب التطبيق %1&apos; فتح المحفظة &apos;%2&apos;. أدخل كلمة المرور أدناه لهذه المحفظة لإلغاء تأمينها</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="74"/>
+        <source>Wallet &apos;%1&apos; does not exist,do you want to create it?</source>
+        <translation>المحفظة &apos;%1&apos; غير موجودة ، هل تريد إنشائها؟</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="90"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Wallet &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; does not exist, do you want to create it?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;المحفظة &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt;غير موجودة ، هل تريد إنشائها؟&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="107"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;An application &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; has requested to open a wallet&lt;span style=&quot; font-weight:600;&quot;&gt; &apos;%2&apos;&lt;/span&gt;. Enter the password below for this wallet to unlock it.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;طلب أحد التطبيقات &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; فتح محفظة&lt;span style=&quot; font-weight:600;&quot;&gt; &apos;%2&apos;&lt;/span&gt;. أدخل كلمة المرور أدناه لهذه المحفظة لفتحها.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="124"/>
+        <source>&amp;Unlock</source>
+        <translation>&amp;الغاء القفل</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="141"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;إلغِ</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="167"/>
+        <source>&amp;Ok</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <location filename="../frontend/password_dialog.ui" line="154"/>
+        <source>ok</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
