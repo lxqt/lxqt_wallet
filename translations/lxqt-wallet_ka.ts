cb185bc327f4cf9c498b245014580d11dfaa3ffa
Weblate <noreply@weblate.org>
2025-03-21 04:25:45 +0100

    Weblate commits
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: Mingxin <tmx2000@126.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/zh_CN/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-wallet/zh_TW/
    Translation: LXQt Desktop/LXQt Wallet

--- translations/lxqt-wallet_ka.ts
+++ translations/lxqt-wallet_ka.ts
@@ -1,38 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko">
+<TS version="2.1" language="ka">
 <context>
     <name>LXQt::Wallet::changePassWordDialog</name>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="108"/>
         <source>Create a new wallet</source>
-        <translation>새 지갑 만들기</translation>
+        <translation>ახალი საფულის შექმნა</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="110"/>
         <source>Create</source>
-        <translation>만들기</translation>
+        <translation>შექმნა</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="138"/>
         <location filename="../frontend/changepassworddialog.cpp" line="280"/>
         <source>Passwords do not match</source>
-        <translation>비밀번호가 일치하지 않습니다</translation>
+        <translation>პაროლები არ ემთხვევა</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="221"/>
         <source>Wallet password could not be changed</source>
-        <translation>지갑 비밀번호를 변경할 수 없습니다</translation>
+        <translation>საფულის პაროლის შეცვლა შეუძლებელია</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="232"/>
         <source>Wallet could not be opened with the presented key</source>
-        <translation>제시된 키로 지갑을 열 수 없습니다</translation>
+        <translation>ამ გასაღებით საფულეს ვერ გახსნით</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.cpp" line="244"/>
         <source>New passwords do not match</source>
-        <translation>새 비밀번호가 일치하지 않습니다</translation>
+        <translation>ახალი პაროლები არ ემთხვევა</translation>
     </message>
 </context>
 <context>
@@ -40,7 +40,7 @@
     <message>
         <location filename="../frontend/password_dialog.cpp" line="72"/>
         <source>Wallet could not be opened with the presented key</source>
-        <translation>제시된 키로 지갑을 열 수 없습니다</translation>
+        <translation>ამ გასაღებით საფულეს ვერ გახსნით</translation>
     </message>
 </context>
 <context>
@@ -48,37 +48,37 @@
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="14"/>
         <source>Change Wallet&apos;s Password</source>
-        <translation>지갑 비밀번호 변경</translation>
+        <translation>საფულის პაროლის შეცვლა</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="42"/>
         <source>C&amp;hange</source>
-        <translation>변경하기(&amp;H)</translation>
+        <translation>შე&amp;ცვლა</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="55"/>
         <source>&amp;Ok</source>
-        <translation>확인(&amp;O)</translation>
+        <translation>&amp;დიახ</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="126"/>
         <source>Enter Current Password Below.</source>
-        <translation>현재 비밀번호를 아래에 입력하세요.</translation>
+        <translation>შეიყვანეთ ქვემოთ მიმდინარე პაროლი.</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="145"/>
         <source>Enter New Password Below.</source>
-        <translation>현재 비밀번호를 아래에 입력하세요.</translation>
+        <translation>შეიყვანეთ ქვემოთ ახალი პაროლი.</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="164"/>
         <source>Re Enter New Password Below.</source>
-        <translation>새 비밀번호를 아래에 다시 입력하세요.</translation>
+        <translation>შეიყვანეთ ქვემოთ ახალი პაროლი კიდევ ერთხელ.</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="183"/>
         <source>&amp;Cancel</source>
-        <translation>취소(&amp;C)</translation>
+        <translation>&amp;გაუქმება</translation>
     </message>
     <message>
         <location filename="../frontend/changepassworddialog.ui" line="196"/>
@@ -92,7 +92,7 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; 응용 프로그램이  &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; 지갑의 비밀번호 변경을 요청했습니다&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;აპლიკაციამ &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; მოითხოვა, რომ პაროლი თავისი საფულისთვის &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; შეცვალოს&lt;/p&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
@@ -106,7 +106,7 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos; 응용 프로그램에 대한 새 지갑 &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos;을(를) 생성하려면 아래에 비밀번호 정보를 입력하세요&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;შეიყვანეთ პაროლის ინფორმაცია ქვემოთ, ახალი საფულის შესაქმნელად &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%1&lt;/span&gt;&apos; აპლიკაციისთვის &apos;&lt;span style=&quot; font-weight:600;&quot;&gt;%2&lt;/span&gt;&apos;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -114,17 +114,17 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../frontend/password_dialog.ui" line="14"/>
         <source>lxqt wallet service</source>
-        <translation>lxqt 지갑 서비스</translation>
+        <translation>lxqt-ის საფულის სერვისი</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="42"/>
         <source>The application &apos;%1&apos; has requested to open the wallet &apos;%2&apos;.Enter the password below for this wallet to unlock it</source>
-        <translation>&apos;%1&apos; 응용 프로그램이 &apos;%2&apos; 지갑 열기를 요청했습니다. 이 지갑을 잠금 해제하려면 아래에 비밀번호를 입력하세요</translation>
+        <translation>აპლიკაციამ &apos;%1&apos; მოითხოვა, გაიხსნას საქაღალდე &apos;%2&apos;. შეიყვანეთ საფულის პაროლი ქვემოთ მისი განბლოკვისთვის</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="74"/>
         <source>Wallet &apos;%1&apos; does not exist,do you want to create it?</source>
-        <translation>&apos;%1&apos; 지갑이 존재하지 않습니다. 지갑을 만드시겠습니까?</translation>
+        <translation>საფულე &apos;%1&apos; არ არსებობს. გნებავთ, შექმნათ ის?</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="90"/>
@@ -137,7 +137,7 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; 지갑이 존재하지 않습니다. 지갑을 만드시겠습니까?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;საფულე &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; არ არსებობს. გნებავთ, შექმნათ ის?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="107"/>
@@ -150,27 +150,27 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; 응용 프로그램이 지갑 &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%2&apos;&lt;/span&gt;열기를 요청했습니다. 이 지갑의 잠금을 해제하려면 아래에 비밀번호를 입력하세요.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;აპლიკაციამ &lt;span style=&quot; font-weight:600;&quot;&gt;&apos;%1&apos;&lt;/span&gt; მოითხოვა გახნა საფულისთვის &lt;span style=&quot; font-weight:600;&quot;&gt; &apos;%2&apos;&lt;/span&gt;. შეიყვანეთ ქვემოთ პაროლი მისი განბლოკვისთვის.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="124"/>
         <source>&amp;Unlock</source>
-        <translation>잠금 해제(&amp;U)</translation>
+        <translation>&amp;განბლოკვა</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="141"/>
         <source>&amp;Cancel</source>
-        <translation>취소(&amp;C)</translation>
+        <translation>გა&amp;უქმება</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="167"/>
         <source>&amp;Ok</source>
-        <translation>확인(&amp;O)</translation>
+        <translation>&amp;დიახ</translation>
     </message>
     <message>
         <location filename="../frontend/password_dialog.ui" line="154"/>
         <source>ok</source>
-        <translation>확인</translation>
+        <translation>დიახ</translation>
     </message>
 </context>
 </TS>
